7caab33ad82498c1fe2ae00d407bc9f06aeac0a4
theonlyrichard <richard.cardoza16@gmail.com>
2022-03-13 16:23:08 -0700

    user-controllers update

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -13,12 +13,89 @@ const usersController = {
     // get all users
     getAllUsers(req, res) {
         users.find({})
-            .populate({ path: "thoughts", select: "-_v" })
-            .populate({ path: "friends", select: "-_v" })
-            .select("-_v")
+            .populate({ path: "thoughts", select: "-__v" })
+            .populate({ path: "friends", select: "-__v" })
+            .select("-__v")
             .then(dbUserData => res.json(dbUserData))
             .cath(err => {
                 console.log(err);
                 res.status(500).json(err);
             });
-    }
+    },
+
+        getUsersByID({ params }, res) {
+    user.findOne({ _id: params.id })
+        .populate({ path: "thoughts", select: "-__v" })
+        .populate({ path: "friends", select: "-__v" })
+        .select("-__v")
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "Unknown ID" });
+                return;
+            }
+            res.json(dbUserData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        })
+},
+//update user by ID
+updateUsers({ params, body }, res) {
+    users.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: " Unknown ID" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err))
+},
+
+deleteUsers({ params }, res) {
+    users.findOneAndDelete({ _id: params, id })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "Unknown ID" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+},
+
+//delete friend
+deleteFriend({ params }, res) {
+    users.findOneAndUpdate({ _id: params.id }, { $pull: { friends: params.friendID } }, { new: true })
+        .populate({ path: "friends", select: "-__v" })
+        .select("-__V")
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "Unknown ID" });
+                return;
+            }
+            res.json(dbUserData);
+
+        })
+        .catch(err => res.status(400).json(err));
+},
+
+//addFriend
+addFriend({ params }, res) {
+    users.findOneAndUpdate({ _id: params.id }, { push: { friends: params.friendID } }, { new: true })
+        .populate({ path: "friends", select: ("-__v") })
+        .select("-__v")
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "Unknown ID" })
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+}
+
+};
+
+module.exports = usersController;
