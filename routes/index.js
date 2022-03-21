2036403976a8d0646d63859259acd0a7d7387f05
theonlyrichard <richard.cardoza16@gmail.com>
2022-03-20 18:59:48 -0700

    fixed structure

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const usersRoutes = require("./user-routes");
+const usersRoutes = require("./api/user-routes");
 const thoughtsRoutes = require("./thought-routes");
 router.use("/users", usersRoutes);
 router.use("/thoughts", thoughtsRoutes);
